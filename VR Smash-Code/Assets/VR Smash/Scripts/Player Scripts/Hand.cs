db975bb7d3270a4d64811a0d7e00d2ed5122d8d7
HugzFnr <hfournier001@ensc.fr>
2023-04-06 17:48:30 +0100

    VR setup
    vr test scene, Hand script start

--- VR Smash-Code/Assets/VR Smash/Scripts/Player Scripts/Hand.cs
+++ VR Smash-Code/Assets/VR Smash/Scripts/Player Scripts/Hand.cs
@@ -1,16 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class VolleyBall_Player : MonoBehaviour
+public class Hand : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
